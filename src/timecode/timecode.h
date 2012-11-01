2bfa0583ad357903cdd6a14ee893166866135d21
Robin Gareus <robin@gareus.org>
2012-11-01 03:42:11 +0100

    checked the SMPTE spec, 29.97 =!= 30000/1001.
    "[..] field rate of an NTSC television signal is 60/1.001 fields per second"
    1 = sec_NTSC = 1.001 sec_REAL

--- src/timecode/timecode.h
+++ src/timecode/timecode.h
@@ -98,15 +98,13 @@ typedef struct Timecode {
 /* TODO: subframe default, use 80, 100 or 384 or 600 or 5760,.. or 500000
  * 84672000 = LCM(192k, 88.2k, 24, 25, 30)
  */
-/* TODO: clear up fractional fps concept - check SMPTE-12M and ATSC specs */
 
 const TimecodeRate tcfps23976   = { 24000, 1001, 0, 80};
 const TimecodeRate tcfps24      = {    24,    1, 0, 80};
 const TimecodeRate tcfps24976   = { 25000, 1001, 0, 80};
 const TimecodeRate tcfps25      = {    25,    1, 0, 80};
 const TimecodeRate tcfps2997ndf = { 30000, 1001, 0, 80};
-const TimecodeRate tcfps30Mdf   = { 30000, 1001, 1, 80};
-const TimecodeRate tcfps2997df  = {30*999, 1000, 1, 80};
+const TimecodeRate tcfps2997df  = { 30000, 1001, 1, 80};
 const TimecodeRate tcfps30      = {    30,    1, 0, 80};
 const TimecodeRate tcfps30df    = {    30,    1, 1, 80};
 const TimecodeRate tcfps5994    = { 60000, 1001, 0, 80};
